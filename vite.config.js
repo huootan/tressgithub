a29331dace5de80827345b192fc14882d2b87209
huootan <huootan@gmail.com>
2026-05-15 19:42:04 +0800

    fix: 移除子模組並修正資料夾結構

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tressgithub/', // 例如：如果您建立的 repository 叫 warehouse，這裡就填 '/warehouse/'
+  // ⚠️ 重要：請將 '您的倉庫名稱' 替換為 GitHub 上的 Repository 名字 (例如 /rope-system/)
+  // 如果是個人首頁 (username.github.io) 則設定為 '/'
+  base: '/tressgithub/', 
 })
